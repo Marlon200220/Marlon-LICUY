8e7ee28d3467175aa71e0e4c99e53e6438e8421c
Marlon Licuy <ma.licuyt@uea.edu.ec>
2024-02-22 18:53:24 -0500

    deber semana 11

---
+++ semana programa 2.py
@@ -0,0 +1,10 @@
+#Pizarra (Whiteboard)
+# Crear una matriz bidimensional de ejemplo
+matriz = [
+    [70, 60, 14],
+    [89, 90, 15],
+    [55, 47, 69]
+]
+for fila in matriz:
+    fila.sort()
+    print(fila)
